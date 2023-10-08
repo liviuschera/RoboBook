6145687ec877fc4c18d5d608c41aea850ecdc151
liviuschera <liviu.schera@gmail.com>
2023-10-08 16:53:48 +0300

    add input, filter and display users functionality

--- src/App.js
+++ src/App.js
@@ -2,32 +2,69 @@ import { Component } from "react";
 
 import "./App.css";
 
+
+
 class App extends Component {
   constructor() {
+    console.log("constructor");
     super();
 
     this.state = {
       users: [],
+      filteredUsers: [],
     };
   }
 
   async componentDidMount() {
-    const users = await fetch(
-      "https://random-data-api.com/api/v2/users?size=11&is_json=true"
-    ).then((res) => res.json());
-    this.setState({ users });
+    console.log("componentDidMount");
+    if (this.state.users.length === 0) {
+      const response = await fetch(
+        "https://random-data-api.com/api/v2/users?size=33&is_json=true"
+      );
+      const users = await response.json();
+      this.setState({ users, filteredUsers: users }, () => console.log("inputval: ", this.state));
+    }
   }
 
   render() {
+    console.log("render");
+
     return (
       <div className="App">
-        {this.state.users.map((user) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search users"
+          onChange={(event) => {
+            console.log("event: ", event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredUsers = this.state.users.filter(user => user.first_name.toLocaleLowerCase().includes(searchString) || user.last_name.toLocaleLowerCase().includes(searchString));
+            this.setState(
+              () => {
+                return { filteredUsers: filteredUsers };
+              },
+              () => {
+                console.log("render => ", this.state);
+              }
+            );
+          }
+          }
+        />
+        {this.state.filteredUsers.map((user) => {
           return (
             <div key={user.uid}>
-              <p>{user.last_name}</p>
+              <span>
+                {user.first_name} {user.last_name}
+              </span>
+              <span>
+                &nbsp;
+              </span>
             </div>
+
           );
-        })}
+        })
+        }
+
       </div>
     );
   }

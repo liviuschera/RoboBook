2635893aadf35fb4cce94829c3db4085fa64458b
liviuschera <liviu.schera@gmail.com>
2023-10-08 08:24:56 +0300

    fectch users data from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      users: [],
+    };
+  }
+
+  async componentDidMount() {
+    const users = await fetch(
+      "https://random-data-api.com/api/v2/users?size=11&is_json=true"
+    ).then((res) => res.json());
+    this.setState({ users });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.users.map((user) => {
+          return (
+            <div key={user.uid}>
+              <p>{user.last_name}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
